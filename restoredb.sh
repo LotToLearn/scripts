337e934fcca530893158f6601d53137ac9257b77
Noah <56550251+LotToLearn@users.noreply.github.com>
2020-11-25 11:35:54 -0600

    Update and rename restore_micros.txt to restoredb.sh

--- restoredb.sh
+++ restoredb.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # Copyright (C) 2020 NOAH HORNER (https://github.com/LotToLearn)
 # Scripts expressed are my own and do not represent anyone else
-
 # steps on Linux
-# vi restore_micros.sh
+# vi restoredb.sh
 # hit i, then paste all of this
 # chmod 777 restore_micros.sh
 # nohup ./restore_micros.sh &
 # hit enter
 # tail -100f nohup.out
-
-
 export NLS_DATE_FORMAT='DD-MON-YY HH24:MI:SS'
 export DATE=$(date +%Y-%m-%d-%H-%M-%S)
 export LOGS_DIR=/home/oracle/
@@ -19,19 +16,15 @@ echo ===========================================================================
 echo "RUNNING FULL DATABASE RESTORE!!!!"
 echo =========================================================================================================
 echo
-
-
 rman target / << EOF | tee ${LOGS_DIR}RMAN_DB_RESTORE_${DATE}.log
+# Allocate channels based on CPUs on the DB
+# allocate channel c5..c6..etc device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
 set controlfile autobackup format for device type sbt to '%F';
 run {
   allocate channel c1 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   allocate channel c2 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   allocate channel c3 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   allocate channel c4 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c5 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c6 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c7 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c8 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   RESTORE DATABASE;
     }
 exit
