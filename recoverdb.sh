7fe3fded14e8613a5dbff362c18d44b620af813b
Noah <56550251+LotToLearn@users.noreply.github.com>
2020-11-25 11:42:40 -0600

    Update and rename recover_micros.sh to recoverdb.sh

--- recoverdb.sh
+++ recoverdb.sh
@@ -3,7 +3,7 @@
 # Scripts expressed are my own and do not represent anyone else
 
 # steps on Linux
-# vi restore_micros.sh
+# vi recoverdb.sh
 # hit i, then paste all of this
 # chmod 777 restore_micros.sh
 # nohup ./restore_micros.sh &
@@ -20,7 +20,7 @@ echo "RUNNING FULL DATABASE RECOVERY!!!!"
 echo =========================================================================================================
 echo
 
-
+# allocate channel c5..c6..etc device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
 rman target / << EOF | tee ${LOGS_DIR}RMAN_DB_RESTORE_${DATE}.log
 set controlfile autobackup format for device type sbt to '%F';
 run {
@@ -28,10 +28,6 @@ run {
   allocate channel c2 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   allocate channel c3 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   allocate channel c4 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c5 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c6 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c7 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
-  allocate channel c8 device type sbt PARMS 'SBT_LIBRARY=/home/oracle/lib/libopc.so, SBT_PARMS=(OPC_PFILE=/home/oracle/config)';
   switch datafile all;
   switch tempfile all;
   RECOVER DATABASE;
